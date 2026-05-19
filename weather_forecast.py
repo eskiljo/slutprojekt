576bcd19cb933b1a71f2d4e021367392615af342
eskiljo <eskil.johansson@edu.huddinge.se>
2026-05-19 16:05:19 +0000

    la till så att matplotlib nu kan plotta grafer och spara dem som filer. Det enda som är kvar nu är att fixa x-labeln på graferna (för mycket text, man kan inte se vad som står), och att göra att man kan se det på flask-appen genom render_template()

--- weather_forecast.py
+++ weather_forecast.py
@@ -2,6 +2,7 @@ import requests
 import json
 import matplotlib.pyplot as plt
 from flask import Flask, render_template, request
+from termcolor import colored
 
 app = Flask(__name__)
 
@@ -10,10 +11,8 @@ forecast_info = "./forecast.txt"
 base_url = "https://api.open-meteo.com/v1/forecast"
 
 
-
 def get_forecast(lat, long, parameters, days):
     url = "https://api.open-meteo.com/v1/forecast"
-    global data
 
     params = {}
     params["latitude"] = lat
@@ -23,9 +22,6 @@ def get_forecast(lat, long, parameters, days):
     parameter_keys = parameters.keys()
 
     joint_params = []
-
-    #print(parameters)
-    #print(parameter_keys)
 
     for parameter in parameter_keys:
         if parameter == "forecast_days":
@@ -44,39 +40,12 @@ def get_forecast(lat, long, parameters, days):
         if "," in parameter:
             #Adds parameters that were meant to be displayed in the same graph to a list
             joint_params.append(parameter) 
-    #print(params)
             
-        
-    #params = {  "latitude": 59.33,
-    #    "longitude": 18.07,
-    #    "hourly": "temperature_2m,relative_humidity_2m",
-    #    "daily": "temperature_2m_mean",
-    #    "forecast_days": 5 #days
-    #}
     response = requests.get(url, params=params)
     data = response.json()
-    #print(data)
+    print(data)
     queue_plot_graphs(data, joint_params)
 
-    #with open("forecast.txt") as forecast_info:
-        #text = json.load(forecast_info)
-        #global data
-        #if not text:
-            #with open("forecast.txt", "w") as forecast_info:
-                #url = "https://api.open-meteo.com/v1/forecast"
-                
-                #params = {  "latitude": 59.33,
-                #            "longitude": 18.07,
-                #            "hourly": "temperature_2m,relative_humidity_2m",
-                #            "daily": "temperature_2m_mean",
-                #            "forecast_days": 5 #days
-               # }
-              #  response = requests.get(url, params=params)
-             #   data = response.json()
-           #     forecast_info.write(json.dumps(data))
-      #  else:
-      #      data = text
-    #print(data)
 
 def get_geolocation(name):
     search_url = "https://geocoding-api.open-meteo.com/v1/search"
@@ -92,97 +61,132 @@ def get_geolocation(name):
     return geolocation_data
 
 
-def queue_plot_graphs(data, joint_params):
+def queue_plot_graphs(data, joint_params): 
     if data.get("hourly"):
         hourly = data["hourly"]
     else:
+        print("NO HOURLY")
         hourly = []
     
     if data.get("daily"):
         daily = data["daily"]
     else:
+        print("NO DAILY")
         daily = []
     
 
     used = set()
 
     for group in joint_params:
+        isdaily = False
+        ishourly = False
         params = group.split(",")
         graph = []
-
+        
         for param in params:
-            for section in data.values(): 
-                if isinstance(section, dict) and param in section: #if section is of type dict
-                    graph.append(section[param])
-                    if section["time"] not in graph:
-                        graph.append(section["time"])
+            if param in daily:
+                graph.append([param, daily[param]])
+                used.add(param)
+                isdaily = True
 
-                    used.add(param)
-        if graph:
-            #plot_graph(graph)
-            #print(graph)
+            elif param in hourly:
+                graph.append([param, hourly[param]])
+                used.add(param)
+                ishourly = True
+
+        if isdaily:
+            graph.append(["time",daily["time"]])
+
+        elif ishourly:
+            graph.append(["time", hourly["time"]])
+
+        plot_graph(graph, data, isdaily, ishourly)
+
+    if daily:
+        for i in daily:
+            if i != "time":
+                if not i in used:
+                    plot_graph([["time", daily["time"]], [i, daily[i]]], data, True, False)
+    if hourly:
+        for i in hourly:
+            if i != "time":
+                if not i in used:
+                    plot_graph([["time", hourly["time"]], [i, hourly[i]]], data, False, True)
 
 
 
-    for item in daily:
-        if item.key() != "time":
-            plot_graph([item, daily["time"]])
-    for item in hourly:
-        if item.key() != "time":
-            plot_graph([item, hourly["time"]])
+def plot_graph(list, data, isdaily, ishourly):
+    plt.figure()
+    colors = ["red", "blue", "green", "yellow"]
+    times = 0
+    title = []
+    title_text = ""
 
 
+    for item in list:
+        if item[0] == "time":
+            time = item[1]
 
-def plot_graph(param_list):
-    parameters = param_list.remove("time")
+    for item in list:
 
-
-    plt.plot(param_list)
-
-    #print(data["hourly"])
-    plt.subplot(2, 1, 1)
-    plt.plot(data["hourly"]["temperature_2m"], color = "red")
-    plt.xlabel("Temperature")
-    plt.ylabel("C°")
-
-    plt.subplot(2, 1, 2)
-    plt.plot(data["hourly"]["relative_humidity_2m"], color = "blue")
-    plt.xlabel("Humidity")
-    plt.ylabel("%")
-
-    #detta är FEL, använd flera grafer och klistra sedan ihop dem till en stor png som användaren kollar på (alternativt en hemsida)
-    #plt.show()
+        if item[0] == "time":
+            pass
+        else: 
+            if len(list) > 2:
+                plt.plot(time, item[1], color = colors[times])
+                times += 1
+                title.append([item[0], colors[times]])
+            else:
+                plt.plot(time, item[1])
+                title.append([item[0], ""])
+        if isdaily:
+            plt.ylabel(data["daily_units"][item[0]])
+        elif ishourly:
+            plt.ylabel(data["hourly_units"][item[0]])
+    if title:
+        for thing in title:
+            if thing[1]:
+                title_text += thing[0] + "(" + thing[1] + ")" + "\n"
+            else:
+                title_text += thing[0]
+        plt.title(title_text)
     plt.tight_layout()
-    plt.savefig("forecast.png")
+    global total_graphs
+    total_graphs += 1
+    plt.savefig("forecast" + str(total_graphs) + ".png")
+
 
 @app.route("/")
 def home():
-    return render_template("selection.html")
+    return render_template("selection.html", error = "")
 
 
 @app.route("/request_form", methods=["POST"])
 def get_request():
+    global total_graphs
+    total_graphs = 0
+
     position = {}
     requested_data = request.form.to_dict()
     for key in ["city", "lat", "long"]:
         if key in requested_data:
             position[key] = requested_data.pop(key)
-    #print(position)
-    #print(requested_data)
+
     if position["city"]:
         coordinates = get_geolocation(position["city"])
-        #print(coordinates)
-        get_forecast(coordinates["results"][0]["latitude"], coordinates["results"][0]["longitude"], requested_data, request.form["forecast_days"])
+
+        if coordinates["results"]:
+            get_forecast(coordinates["results"][0]["latitude"], coordinates["results"][0]["longitude"], requested_data, request.form["forecast_days"])
+        else: 
+            render_template("selection.html", error = "Try again")
+
     elif position["lat"] and position["long"]:
         get_forecast(position["lat"], position["long"], requested_data, request.form["forecast_days"])
+
     else:
-        return render_template("selection.html")
+        return render_template("selection.html", error = "")
     return render_template("weather.html")
 
 
-#get_forecast()
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-#status_code
